88e11db9453542dfbc758c42ba7813553dceefef
jaseel <jaseeljctcs@gmail.com>
2022-12-03 23:40:56 +0530

    navbar in viewfrnds

---
+++ src/app/view-frnd/view-frnd.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-frnd',
+  templateUrl: './view-frnd.component.html',
+  styleUrls: ['./view-frnd.component.css']
+})
+export class ViewFrndComponent {
+
+}
