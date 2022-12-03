586c8f36828a6b7c0b25e946de6efce618c7b993
jaseel <jaseeljctcs@gmail.com>
2022-12-03 21:38:45 +0530

    about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const myroute:Routes=[
   {
@@ -16,7 +17,8 @@ const myroute:Routes=[
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
